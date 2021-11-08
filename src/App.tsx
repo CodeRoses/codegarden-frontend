30a7352e0ee3a26b6f8473f96ffc47dac8cbea7f
Mateusz Roszkowski <blackspider1@o2.pl>
2021-11-08 10:35:14 +0100

    Added router, eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-function App() {
+const Placeholder = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Witamy w Codegarden. Baw się dobrze.
-        </p>
+        <p>Witamy w Codegarden. Baw się dobrze.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +18,25 @@ function App() {
         </a>
       </header>
     </div>
+  );
+};
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Placeholder />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
